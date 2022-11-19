b798fad4acb918482c42ffe3379b88bf5e6a4f8f
Bazingadoodoo <blonicle1130@gmail.com>
2022-11-19 13:16:57 -0500

    Direction
    functions were updated with proper directions included

--- Codes/Navigation_Robot_Code_Temlpate/Navigation_Robot_Code_Temlpate.ino
+++ Codes/Navigation_Robot_Code_Temlpate/Navigation_Robot_Code_Temlpate.ino
@@ -9,17 +9,17 @@
 #define echoPin 5   
 
 // motor driver----------------------------
-#define enA 10      
-#define enB 11      
-#define in1 12      
-#define in2 13      
-#define in3 8       
-#define in4 9       
+#define enA 10      // right wheel
+#define enB 11      // left wheel
+#define in1 12      // forward
+#define in2 13      // backward
+#define in3 8       // backward
+#define in4 9       // forward
 
 Servo myservo;
 
 void setup() {
-  myservo.attach(servoPin);
+  TurnLeft(100,3);
 }
 
 void loop() {
@@ -28,42 +28,42 @@ void loop() {
 
 void TurnRight(int motorSpeed, double delayTime){
   digitalWrite(in1, LOW);
-  digitalWrite(in2, LOW);
+  digitalWrite(in2, HIGH);
   digitalWrite(in3, LOW);
-  digitalWrite(in4, LOW);
+  digitalWrite(in4, HIGH);
   analogWrite(enA, motorSpeed);
   analogWrite(enB, motorSpeed);
-  delay(delayTime)
+  delay(delayTime);
 }
 
 void TurnLeft(int motorSpeed, double delayTime){
-  digitalWrite(in1, LOW);
+  digitalWrite(in1, HIGH);
   digitalWrite(in2, LOW);
-  digitalWrite(in3, LOW);
+  digitalWrite(in3, HIGH);
   digitalWrite(in4, LOW);
   analogWrite(enA, motorSpeed);
   analogWrite(enB, motorSpeed);
-  delay(delayTime)
+  delay(delayTime);
 }
 
 void DriveForward(int motorSpeed, double delayTime){
-  digitalWrite(in1, LOW);
+  digitalWrite(in1, HIGH);
   digitalWrite(in2, LOW);
   digitalWrite(in3, LOW);
-  digitalWrite(in4, LOW);
+  digitalWrite(in4, HIGH);
   analogWrite(enA, motorSpeed);
   analogWrite(enB, motorSpeed);
-  delay(delayTime)
+  delay(delayTime);
 }
 
-void DriveForward(int motorSpeed, double delayTime){
+void DriveBackward(int motorSpeed, double delayTime){
   digitalWrite(in1, LOW);
-  digitalWrite(in2, LOW);
-  digitalWrite(in3, LOW);
+  digitalWrite(in2, HIGH);
+  digitalWrite(in3, HIGH);
   digitalWrite(in4, LOW);
   analogWrite(enA, motorSpeed);
   analogWrite(enB, motorSpeed);
-  delay(delayTime)
+  delay(delayTime);
 }
 
 void TurnOff(){
