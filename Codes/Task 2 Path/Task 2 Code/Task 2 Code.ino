71cf829f10d7cbe6d935143673699472a9eb4ce7
nillannimal <56417260+nillannimal@users.noreply.github.com>
2022-11-20 19:40:49 -0500

    Odometry Function

--- Codes/Task 2 Path/Task 2 Code/Task 2 Code.ino
+++ Codes/Task 2 Path/Task 2 Code/Task 2 Code.ino
@@ -2,19 +2,22 @@
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BNO055.h>
 #include <utility/imumaths.h>
+#include <Smoothed.h>
+
+Smoothed <float> average_rot_speedL;
+Smoothed <float> average_rot_speedR;
 Adafruit_BNO055 bno = Adafruit_BNO055(55);
 float angle_offset;
 float angle;
 
 // encoder---------------------------------
-#include <TimerOne.h>
 #define leftEncoderPin 2
 #define rightEncoderPin 3
 float diskSlot = 20;
 int leftCounter = 0;
 int rightCounter = 0;
-int Right = 0;
-int Left = 0;
+int Right = 1;
+int Left = 1;
 
 // ultrasonic sensor-----------------------
 #define trigPin 4
@@ -25,11 +28,24 @@ int Left = 0;
 #define servoPin 6
 Servo myservo;*/
 
-// IR receiver-----------------------------
-#include <IRremote.hpp>
-#define irPin 11
-IRrecv irReceive(irPin);
-decode_results irInput;
+//Encoder variables
+volatile float currentEncoderR;
+volatile float oldPositionR = 0;
+volatile float newPositionR = 0;
+volatile float rot_speedR;
+volatile int prevEncoderR =0;
+volatile int prevEncoderL =0; 
+
+//Time variables
+float previousTimeR = 0;
+float currentTimeR = 0;
+float previousTimeL = 0;
+float currentTimeL = 0;
+
+volatile long currentEncoderL;
+volatile float oldPositionL = 0;
+volatile float newPositionL = 0;
+volatile float rot_speedL;
 
 // motor driver----------------------------
 #define enA 9       // right wheel
@@ -38,6 +54,12 @@ decode_results irInput;
 #define in2 13      // backward
 #define in3 7       // backward
 #define in4 8       // forward
+
+// Odometry-----
+float R = 0.03325;
+float posX;
+float posY;
+float meanDistance;
 
 void ISR_Left(){
   if (Left){
@@ -121,26 +143,39 @@ float measure_angle(void)
   sensors_event_t event; 
   bno.getEvent(&event);
   z_angle = event.orientation.x; //get z-axis rotation angle
-  Serial.println(z_angle);
   return z_angle;
 }
 
-void Odometry(float offsetR, float offsetL) {  
-  float R = 0.03325;
 
-  float SR = ((rightCounter - offsetR)/20)*(2*PI*R);
-  float SL = ((leftCounter - offsetL)/20)*(2*PI*R);
-    
-  float meanDistance = (SR+SL)/2;
 
-  return meanDistance;
+void Odometry(float theta) {
+
+  float SR = (float)((rightCounter-prevEncoderR)/40)*(2*PI*R);
+  float SL = (float)((leftCounter-prevEncoderL)/40)*(2*PI*R);
+
+  Serial.print(rightCounter);
+  Serial.print(" ");
+  Serial.print(prevEncoderR);
+  Serial.println();
+
+  prevEncoderL = leftCounter;
+  prevEncoderR = rightCounter;
+
+  meanDistance = (SL + SR) / 2;
+  posX = posX + meanDistance * cos (theta);
+  posY = posY + meanDistance * sin(theta);
+
+  Serial.print(rightCounter);
+  Serial.print(" ");
+  Serial.print(leftCounter);
+  Serial.println();
+
 }
 
 void setup() {
   Serial.begin(9600);
   attachInterrupt(digitalPinToInterrupt(leftEncoderPin),ISR_Left,RISING);
   attachInterrupt(digitalPinToInterrupt(rightEncoderPin),ISR_Right,RISING);
-  irReceive.enableIRIn();
   Serial.println("Calibrating IMU");
 
   /* Initialise the sensor */
@@ -149,7 +184,6 @@ void setup() {
     /* There was a problem detecting the imu */
     Serial.print("no imu sensor detected");
     while(1);
-    
     delay(2000);
     bno.setExtCrystalUse(true);
     Serial.println("Done Calibrating");
@@ -158,38 +192,13 @@ void setup() {
     bno.getEvent(&event);
     angle_offset = event.orientation.x; //get z-axis rotation angle
     Serial.println(angle_offset);
+    average_rot_speedL.begin(SMOOTHED_AVERAGE,10); //prepare smoothing library 
+    average_rot_speedR.begin(SMOOTHED_AVERAGE,10); //prepare smoothing library 
   }
 }
 
 void loop() {
-    if (irReceive.decode(&irInput)){
-      int irReading = irInput.value;
-      switch(irReading){
-        case 6375:
-          DriveForward(255,1);
-          break;
-        case 19125:
-          DriveBackward(255,1);
-          break;
-        case 4335:
-          TurnLeft(100,1);
-          break;
-        case 23205:
-          TurnRight(100,1);
-          break;
-        case 14535:
-          TurnOff();
-          break;
-    }
-    irReading = 0;
-      irReceive.resume();
-  }
- 
   angle = measure_angle();
-  //Serial.print("left counter: ");
-  //Serial.print(leftCounter);
- //Serial.print("  //  ");
-  //erial.print("right counter: ");
-  //Serial.println(rightCounter);
-  //TurnLeft(100,1);
+  Odometry(angle);
+
 }
